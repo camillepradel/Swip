2fa1f220683c5896795f0c624bdc2d2af0cc71c0
Sam <sam@samPC.localdomain>
2012-04-27 00:18:21 +0200

    Interface (Requêtes dans les onglets, ...)

--- SwipWebClient/js/app.js
+++ SwipWebClient/js/app.js
@@ -25,13 +25,12 @@ $(function()
 	$('#results').tabs();
 });
 
-function fillTab(data)
+function fillTab()
 {
-
 	jQuery('#jqGrid').jqGrid
 	({
 		datatype: 'jsonstring',
-		datastr: data,
+		datastr: parsedJson,
 		loadonce: true,
 		height: 'auto',
 		autowidth: true,
@@ -41,27 +40,41 @@ function fillTab(data)
 			root: 'content',
 			repeatitems: false,
 		},
-	   	colNames: ['Desc','Rel'],
+	   	colNames: ['Id', 'Desc','Rel'],
 	   	colModel:
 	   	[
-   			{name: 'descriptiveSentence', index: 'descriptiveSentence', width: 80, classes: 'descriptiveSentence'},
-   			{name: 'relevanceMark',index: 'relevanceMark', width: 20, sorttype: 'number', classes: 'relevanceMark'}
+   			{name: 'id', index: 'id', width: 3, classes: 'id', sortable: false},
+   			{name: 'descriptiveSentence', index: 'descriptiveSentence', width: 82, classes: 'descriptiveSentence'},
+   			{name: 'relevanceMark',index: 'relevanceMark', width: 15, sorttype: 'number', classes: 'relevanceMark'}
    		],
    		multiselect: false,
 	   	subGrid: true,
 	   	subGridRowExpanded: function(subgrid_id, row_id) 
 	   	{
-            $('#' + subgrid_id).append(data.content[row_id].sparqlQuery);
+	   		var queryPre = '<span class="title">SPARQL Query :</span><pre class="queryPre"></pre>';
+	   		var mappingPre = '<span class="title">Mapping Description :</span><br /><br /><span class="mappingSpan"></span>';
+            $('#' + subgrid_id).addClass('subgrid');
+            $('#' + subgrid_id).append(queryPre + '<hr />' + mappingPre);
+            $('#' + subgrid_id + ' .queryPre').text(parsedJson.content[row_id].sparqlQuery);
+            $('#' + subgrid_id + ' .mappingSpan').text(parsedJson.content[row_id].mappingDescription);
         },
+        gridComplete: function()
+        {
+			var ids = jQuery("#jqGrid").jqGrid('getDataIDs');
+			for(var i = 0; i < ids.length; i++)
+			{
+				jQuery("#jqGrid").jqGrid('setRowData', ids[i], {id: ids[i]});
+			}
+		},
         pager: '#pager',
         rowNum: 20
 	});
 }
 
-function displayResults(result)
+function displayResults()
 {
 	$('#results').css('display', 'block');
-	fillTab(result);
+	fillTab();
 }
 
 
@@ -77,9 +90,13 @@ function search(query)
 
 function selectQuery(id)
 {
-	/*console.log('#jqGrid_' + id);
-	$('#results').tabs('add', '#jqGrid_' + id, 'Request');*/
-	alert("Not implemented yet !");
+	var sparqlQuery = '<span class="title">SPARQL Query :</span><pre class="queryPre"></pre>';
+	var sparqlResult = '<span class="title">SPARQL Result :</span><pre class="resultPre">Not implemented yet !</pre>'
+	$('#results').append('<div id="query' + id + '" class="query">' + sparqlQuery + '<hr />' + sparqlResult + '</div>');
+	$('#query' + id + ' .queryPre').text(parsedJson.content[id].sparqlQuery);
+	$('#query' + id).css('height', $('#tabs-1').css('height'));
+	$('#results').tabs('add', '#query' + id, 'Query #' + id);
+
 }
 
 function nlToPivotSuccHandler(data, status, req) 
@@ -100,7 +117,8 @@ function pivotToSparqlSuccHandler(data, status, req)
 		}, 'slow', function()
 		{
 			$('#searchButton').removeAttr('disabled');
-			displayResults($.parseJSON($(req.responseXML).find('generateBestMappingsResponse').text()));
+			parsedJson = $.parseJSON($(req.responseXML).find('generateBestMappingsResponse').text());
+			displayResults();
 		});
 	}
 }
