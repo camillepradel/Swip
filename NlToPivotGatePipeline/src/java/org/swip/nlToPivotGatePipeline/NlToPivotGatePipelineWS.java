d297cff3241150f7c2bba2f17f90ec14b2cd0346
Camille Pradel <camillepradel@gmail.com>
2012-05-03 17:21:00 +0200

    ajout de la méthode getQueryWithGatheredNamedEntities issue de l ancien projet NlToPivotGatePipelineFr. Pour l instant, ce sont deux pipelines Gate différents qui sont associés à chacune des deux méthodes fournies par le web service.

--- NlToPivotGatePipeline/src/java/org/swip/nlToPivotGatePipeline/NlToPivotGatePipelineWS.java
+++ NlToPivotGatePipeline/src/java/org/swip/nlToPivotGatePipeline/NlToPivotGatePipelineWS.java
@@ -7,6 +7,7 @@ package org.swip.nlToPivotGatePipeline;
 import gate.*;
 import gate.creole.ExecutionException;
 import gate.creole.ResourceInstantiationException;
+import gate.util.GateException;
 import gate.util.persistence.PersistenceManager;
 import java.io.File;
 import java.util.ArrayList;
@@ -24,50 +25,61 @@ import org.apache.log4j.Logger;
 public class NlToPivotGatePipelineWS {
 
     /** The Corpus Pipeline application */
-    static private CorpusController corpusController = null;
+    static private CorpusController corpusControllerSupple = null;
+    static private CorpusController corpusControllerGazetteer = null;
     private static final Logger logger = Logger.getLogger(NlToPivotGatePipelineWS.class);
 
-    private CorpusController getCorpusController() {
-        //logger.info("la");
-        if (corpusController == null) {
-            // initialise the GATE library
+    private CorpusController getCorpusControllerSupple() {
+        if (corpusControllerSupple == null) {
             try {
-                logger.info("Initialising GATE...");
-                
-                //Gate.setGateHome(new File("/home/sam/Documents/Stage/Swip/swip/NlToPivotGatePipeline/web/WEB-INF"));
-                File userGateFile = new File(this.getClass().getClassLoader().getResource("/../user-gate.xml").getPath());
-                File gateHomeFile = userGateFile.getParentFile();
-                
-                Gate.setGateHome(gateHomeFile);
-                Gate.setUserConfigFile(userGateFile);
-                
-                /*logger.info("USER GATE : "+userGateFile.getAbsolutePath());
-                logger.info("GATE HOME : "+gateHomeFile.getAbsolutePath());*/
-                
-                
-                Gate.init();
-                
-                //logger.info("OKKKKK");
-                
-                logger.debug("default user config file: " + Gate.getDefaultUserConfigFileName());
-                logger.debug("default user session file: " + Gate.getDefaultUserSessionFileName());
-                logger.debug("user config file: " + Gate.getUserConfigFile());
-                logger.debug("user session file: " + Gate.getUserSessionFile());
-                logger.debug("site config file: " + Gate.getSiteConfigFile());
-                logger.debug("plugins home: " + Gate.getPluginsHome());
-                logger.debug("original user config: " + Gate.getOriginalUserConfig());
-                logger.debug("known plugins: " + Gate.getKnownPlugins());
-                logger.debug("built in creole dir: " + Gate.getBuiltinCreoleDir());
-                logger.info("Loading Gate process pipeline...");
-                //FIXME: not the good way to reference static files on a server
-                corpusController = (CorpusController) PersistenceManager.loadObjectFromFile(new File(this.getClass().getClassLoader().getResource("../NlToPivotSupple.gapp").getPath()));
+                if (corpusControllerGazetteer == null) {
+                    initGate();
+                }
+                corpusControllerSupple = (CorpusController) PersistenceManager.loadObjectFromFile(new File(this.getClass().getClassLoader().getResource("../NlToPivotSupple.gapp").getPath()));
                 logger.info("Process pipeline loaded");
             } catch (Exception ex) {
                 logger.error(ex.getStackTrace());
             }
         }
-        logger.info("2");
-        return corpusController;
+        return corpusControllerSupple;
+    }
+
+    private CorpusController getCorpusControllerGazetteer() {
+        if (corpusControllerGazetteer == null) {
+            try {
+                if (corpusControllerSupple == null) {
+                    initGate();
+                }
+                corpusControllerGazetteer = (CorpusController) PersistenceManager.loadObjectFromFile(new File(this.getClass().getClassLoader().getResource("../NlToPivotGazetteer.gapp").getPath()));
+                logger.info("Process pipeline loaded");
+            } catch (Exception ex) {
+                logger.error(ex.getStackTrace());
+            }
+        }
+        return corpusControllerGazetteer;
+    }
+
+    /**
+     * initialize the GATE library
+     * @throws GateException 
+     */
+    private void initGate() throws GateException {
+        logger.info("Initialising GATE...");
+        File userGateFile = new File(this.getClass().getClassLoader().getResource("/../user-gate.xml").getPath());
+        File gateHomeFile = userGateFile.getParentFile();
+        Gate.setGateHome(gateHomeFile);
+        Gate.setUserConfigFile(userGateFile);
+        Gate.init();
+        logger.debug("default user config file: " + Gate.getDefaultUserConfigFileName());
+        logger.debug("default user session file: " + Gate.getDefaultUserSessionFileName());
+        logger.debug("user config file: " + Gate.getUserConfigFile());
+        logger.debug("user session file: " + Gate.getUserSessionFile());
+        logger.debug("site config file: " + Gate.getSiteConfigFile());
+        logger.debug("plugins home: " + Gate.getPluginsHome());
+        logger.debug("original user config: " + Gate.getOriginalUserConfig());
+        logger.debug("known plugins: " + Gate.getKnownPlugins());
+        logger.debug("built in creole dir: " + Gate.getBuiltinCreoleDir());
+        logger.info("Loading Gate process pipeline...");
     }
 
     /**
@@ -77,8 +89,7 @@ public class NlToPivotGatePipelineWS {
     public String getSuppleSemanticAnnotations(@WebParam(name = "adaptedNlQuery") String adaptedNlQuery) {
         logger.info("adapted query for Gate pipeline: " + adaptedNlQuery);
         try {
-            //logger.info("ici");
-            CorpusController cont = this.getCorpusController();
+            CorpusController cont = this.getCorpusControllerSupple();
             Corpus corpus = (Corpus) Factory.createResource("gate.corpora.CorpusImpl");
             Document doc = (Document) Factory.newDocument(adaptedNlQuery);
             ((List<Document>) corpus).add(doc);
@@ -100,6 +111,38 @@ public class NlToPivotGatePipelineWS {
             }
             logger.info("supple semantic output:\n" + suppleSemanticOutput);
             return suppleSemanticOutput;
+        } catch (ExecutionException ex) {
+            logger.error(ex);
+        } catch (ResourceInstantiationException ex) {
+            logger.error(ex);
+        }
+        return "fail to parse adapted nl query";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getQueryWithGatheredNamedEntities")
+    public String getQueryWithGatheredNamedEntities(@WebParam(name = "adaptedNlQuery") String adaptedNlQuery) {
+        logger.info("adapted query for Gate pipeline: " + adaptedNlQuery);
+        try {
+            CorpusController cont = this.getCorpusControllerGazetteer();
+            Corpus corpus = (Corpus) Factory.createResource("gate.corpora.CorpusImpl");
+            Document doc = (Document) Factory.newDocument(adaptedNlQuery);
+            ((List<Document>) corpus).add(doc);
+            cont.setCorpus(corpus);
+            cont.execute();
+            AnnotationSet annotSet = doc.getAnnotations();
+            AnnotationSet lookupToKeepAnnotSet = annotSet.get("LookupToKeep");
+            String resultNLQuery = adaptedNlQuery;
+            for (Annotation lookupToKeepAnnot : lookupToKeepAnnotSet) {
+                logger.info(lookupToKeepAnnot);
+                int start = lookupToKeepAnnot.getStartNode().getOffset().intValue();
+                int end = lookupToKeepAnnot.getEndNode().getOffset().intValue();
+                resultNLQuery = resultNLQuery.substring(0, start) + resultNLQuery.substring(start, end).replaceAll(" ", "_") + resultNLQuery.substring(end);
+                logger.info(resultNLQuery);
+            }
+            return resultNLQuery;
         } catch (ExecutionException ex) {
             logger.error(ex);
         } catch (ResourceInstantiationException ex) {
